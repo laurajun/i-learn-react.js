5ba4e06f41c1c1a42d028a8f37ec2fd2464aa131
Laura J <laura.juntunen@student.hamk.fi>
2020-03-21 03:40:28 +0200

    Counter app before hooks
    This very simply counter app was created using class. 
    Next version with hooks.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	state = {
+		count: 0
+	};
+	increment = () => {
+		this.setState({
+			count: this.state.count + 1
+		});
+	};
+	
+	render() {
+		return (
+			<div> 
+				<h2>counter app</h2>
+				<button onClick={this.increment}>
+					Clicked {this.state.count} times
+				</button>
+			</div>
+		);
+	}
+}
+
+export default App;
